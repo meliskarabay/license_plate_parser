03c174117f8b1bd4d781dbfc55fab0926a70aa17
melis <melisskarabay@gmail.com>
2019-07-31 11:55:05 +0300

    Add gem folder

---
+++ license_plate_parser/test/license_plate_parser_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class LicensePlateParserTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::LicensePlateParser::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
---
+++ license_plate_parser/test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "license_plate_parser"
+
+require "minitest/autorun"
